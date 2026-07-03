20315e0336ca5a70bc627ec10c1801ef27ff4802
Frederik Bark <bark@sim.tu-darmstadt.de>
2026-07-03 13:18:18 +0200

    copyright notice update

--- hector_pointcloud_io/include/hector_pointcloud_io/continuous_pointcloud_saver_node.hpp
+++ hector_pointcloud_io/include/hector_pointcloud_io/continuous_pointcloud_saver_node.hpp
@@ -1,3 +1,5 @@
+// Original Copyright (c) 2025 Stefan Fabian. All rights reserved.
+// Extended version by Frederik Bark
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #ifndef HECTOR_POINTCLOUD_IO_CONTINUOUS_POINTCLOUD_SAVER_NODE_HPP
